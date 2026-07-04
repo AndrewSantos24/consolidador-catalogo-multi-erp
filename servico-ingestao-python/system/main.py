586c320e50dcfcb2aa0ae7deab7f95a5531f757b
Andrew Benvenuto Dias Dos Santos <andrew_santos@MacBook-Air-de-Andrew.local>
2026-07-04 00:18:50 -0300

    feat: main de inicializacao servidor fast criada

---
+++ servico-ingestao-python/system/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+from system.api.v1.routes.health_routes import router as health_router
+from system.core.config import configuracoes
+
+
+app = FastAPI(
+    title=configuracoes.nome_aplicacao,
+    version="0.1.0",
+)
+
+
+app.include_router(health_router, prefix="/api/v1")
