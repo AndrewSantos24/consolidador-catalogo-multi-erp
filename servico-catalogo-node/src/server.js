b0ccf4843b4181685d19e2295ffdaf98fb6a8f61
Andrew Benvenuto Dias Dos Santos <andrew_santos@MacBook-Air-de-Andrew.local>
2026-07-06 10:21:46 -0300

    feat:server atualizado com nova rota de saude do banco

--- servico-catalogo-node/src/server.js
+++ servico-catalogo-node/src/server.js
@@ -2,12 +2,14 @@ const express = require("express");
 
 const env = require("./config/env");
 const healthRoutes = require("./routes/healthRoutes");
+const bancoRoutes = require("./routes/bancoRoutes");
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/api/v1", healthRoutes);
+app.use("/api/v1", bancoRoutes);
 
 app.listen(env.porta, () => {
   console.log(`${env.nomeServico} rodando na porta ${env.porta}`);
