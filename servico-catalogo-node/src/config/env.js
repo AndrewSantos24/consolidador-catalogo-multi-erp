e492e9e3a9c453bb11c9fecc7fb6744ebe8eb318
Andrew Benvenuto Dias Dos Santos <andrew_santos@MacBook-Air-de-Andrew.local>
2026-07-06 22:14:21 -0300

    feat:adicionando variavel tag overview no env

--- servico-catalogo-node/src/config/env.js
+++ servico-catalogo-node/src/config/env.js
@@ -6,8 +6,8 @@ const env = {
   ambiente: process.env.AMBIENTE || "desenvolvimento",
   databaseUrl: process.env.DATABASE_URL,
   rabbitmqUrl: process.env.RABBITMQ_URL,
-  rabbitmqFilaProdutos:
-    process.env.RABBITMQ_FILA_PRODUTOS || "catalogo.produtos.normalizados",
+  rabbitmqConsumerTag: process.env.RABBITMQ_CONSUMER_TAG || "servico-catalogo-node-produtos",
+  rabbitmqFilaProdutos: process.env.RABBITMQ_FILA_PRODUTOS || "catalogo.produtos.normalizados",
 };
 
 module.exports = env;
