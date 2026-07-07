72b6ea8b448ea35b9324431103e6148590887f09
Andrew Benvenuto Dias Dos Santos <andrew_santos@MacBook-Air-de-Andrew.local>
2026-07-06 22:39:57 -0300

    feat:criado enum dos txt e xmls para caso algum campo seja alterado fique facil a alteracao

---
+++ servico-ingestao-python/system/constants/campos_erp_xml.py
@@ -0,0 +1,11 @@
+class CamposErpXml:
+    """Armazena os nomes dos campos recebidos no XML do ERP."""
+
+    PRODUTO = "product"
+    ID = "id"
+    SKU = "sku"
+    NOME = "name"
+    PRECO_LISTA = "price/list"
+    PRECO_DESCONTO = "price/discount"
+    CATEGORIA = "category"
+    ATUALIZADO_EM = "lastUpdate"
