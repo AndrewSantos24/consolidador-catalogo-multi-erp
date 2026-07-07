9a28b12f0913299292b8e13f79fb3b2615293b50
Andrew Benvenuto Dias Dos Santos <andrew_santos@MacBook-Air-de-Andrew.local>
2026-07-06 22:10:14 -0300

    bugfix:corrigindo nome do template do painel html

--- servico-catalogo-node/src/routes/painelmidiaRoutes.js
+++ servico-catalogo-node/src/routes/painelmidiaRoutes.js
@@ -17,7 +17,7 @@ router.get("/painel-midias", (req, res) => {
     __dirname,
     "..",
     "templates",
-    "painelMidias.html"
+    "painelmidias.html"
   );
 
   return res.sendFile(caminhoTemplate);
